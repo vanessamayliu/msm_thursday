916a6099ca02edd536501917715424729967b973
anonymous anonymous <vanessamayliu@gmail.com>
2022-02-24 01:04:48 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,14 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :director_id, :integer
+  attribute :year, :integer
+  attribute :description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
