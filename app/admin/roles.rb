2e925b1b2fbb38c9e1cb0725940f45c26d97486f
anonymous anonymous <vanessamayliu@gmail.com>
2022-02-24 01:04:17 +0000

    Whitelist columns in Role for ActiveAdmin

--- app/admin/roles.rb
+++ app/admin/roles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Role do
 
+ permit_params :movie_id, :character, :actor_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
