ade5a131f4083579e9d0ae09769f6f2485c6a740
anonymous anonymous <vanessamayliu@gmail.com>
2022-02-24 01:04:27 +0000

    Display associated collections on show pages

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -42,8 +42,14 @@ class RolesController < ApplicationController
   # DELETE /roles/1
   def destroy
     @role.destroy
-    redirect_to roles_url, notice: 'Role was successfully destroyed.'
+    message = "Role was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to roles_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
