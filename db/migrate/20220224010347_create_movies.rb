0fc49ef5f2709265aabf8a82da6f02577e699b58
anonymous anonymous <vanessamayliu@gmail.com>
2022-02-24 01:03:49 +0000

    Generate movies resource with ideas:scaffold

---
+++ db/migrate/20220224010347_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :director_id
+      t.integer :year
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
